89b2ec5f8a017db63ec8fd1412765852400b25a1
Alex Darigan <code@darigan.ie>
2021-02-13 18:28:23 +0000

    Create project
    - added WAT
    - added sample scene
    - added sample test
    - add test workflow

---
+++ Tests/SampleTest.cs
@@ -0,0 +1,12 @@
+namespace CardGame.Tests
+{
+    public class SampleTest : WAT.Test
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(true, "TestRunner Works");
+        }
+
+    }
+}
