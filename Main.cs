340bf7d497d8c96fcc573fa43ffae444fbe96457
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2021-02-13 18:28:23 +0000

    Create project
    - added WAT
    - added sample scene
    - added sample test
    - add test workflow

---
+++ Main.cs
@@ -0,0 +1,14 @@
+using System;
+using Godot;
+
+namespace CardGame
+{
+    public class Main : Node
+    {
+        public override void _Ready()
+        {
+            Console.WriteLine("Hello World");
+            GD.Print("Hello World");
+        }
+    }
+}
