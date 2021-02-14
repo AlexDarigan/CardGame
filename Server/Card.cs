35a1cf8318f3b0509462897a4c74f70a175aa358
Alex Darigan <code@darigan.ie>
2021-02-14 23:22:16 +0000

    Add Simple Card

---
+++ Server/Card.cs
@@ -0,0 +1,14 @@
+﻿namespace CardGame.Server
+{
+    public class Card
+    {
+        public readonly int Id;
+        private readonly Player Owner;
+
+        public Card(int id, Player owner)
+        {
+            Id = id;
+            Owner = owner;
+        }
+    }
+}
