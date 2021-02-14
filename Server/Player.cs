e0cbd749cd774e9f4c6cb7fd8d6a9bb3b89ebde0
Alex Darigan <code@darigan.ie>
2021-02-14 23:22:44 +0000

    Add Deck Property To Player

--- Server/Player.cs
+++ Server/Player.cs
@@ -1,10 +1,13 @@
-﻿namespace CardGame.Server
+﻿using System.Collections.Generic;
+
+namespace CardGame.Server
 {
     public class Player
     {
         public readonly int Id;
         public Player Opponent;
         public bool Ready = false;
+        public readonly List<Card> Deck = new List<Card>();
 
         public Player(int id)
         {
