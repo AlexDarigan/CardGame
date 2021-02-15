77c0cbb3ae908adf613e6b847cc7b505498b8a69
Alex Darigan <code@darigan.ie>
2021-02-15 00:03:17 +0000

    Add Comments About Function Invocation.
    We have two primary type of scripts. The first is
    our action invoker scripts, like our player or in future, skills.
    
    These invoker scripts will have public methods that are accessible
    from our tests or other controller scripts. This allows us to test the
    effects of actions without worrying about setting our project up just
    right for each test.
    
    Contrary to this is our controller scripts, such as match. When invoking
    actions from these scripts, they will first check to make sure everything
    is legal otherwise they will disqualify the player committing the illegal
    play.

--- Server/Match.cs
+++ Server/Match.cs
@@ -2,6 +2,12 @@
 {
     public class Match
     {
+        /*
+         * Match is a Rules-Enforced Script. This means any action called from here will have
+         * to be a legal play otherwise the player invoking the illegal play will be disqualified.
+         * When we want to test actions independently of a script like this, we will just invoke them
+         * directly on their owners (either a player or a skill).
+         */
         public Match()
         {
             
--- Server/Player.cs
+++ Server/Player.cs
@@ -2,6 +2,12 @@
 
 namespace CardGame.Server
 {
+    /*
+     * Player is a Script that doesn't care about the game rules unlike Match
+     * Largely that means what can happen here depends on who is invoking it (either
+     * the game or a skill from a card). This allows us to test a number of actions with
+     * ease in our Unit Tests
+     */
     public class Player
     {
         public readonly int Id;
