536af8e701064e0b9c7d5bbde8b4be56bfb259cc
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2021-02-19 23:35:02 +0000

    Add Regions To VM
    Far better than using a partial class like I was planning too. Keeps
    code tidy and still isolated.

--- Server/VirtualStackMachine.cs
+++ Server/VirtualStackMachine.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
-using System.Threading;
 
 namespace CardGame.Server
 {
@@ -33,89 +31,88 @@ namespace CardGame.Server
 
 		}
 
-		private Action GetOperation(Instructions instruction) => instruction switch
+		private Action GetOperation(Instructions instruction)
 		{
-			// Getters
-			Instructions.Literal => Literal,
-			Instructions.GetOwningCard => GetOwningCard,
-			Instructions.GetController => GetController,
-			Instructions.GetOpponent => GetOpponent,
-			Instructions.GetDeck => GetDeck,
-			Instructions.GetGraveyard => GetGraveyard,
-			Instructions.GetHand => GetHand,
-			Instructions.GetUnits => GetUnits,
-			Instructions.GetSupport => GetSupports,
-			Instructions.Count => Count,
-			
-			// Control Flow
-			Instructions.If => If,
-			Instructions.GoToEnd => GoToEnd,
-
-			// Boolean
-			Instructions.IsLessThan => IsLessThan,
-			Instructions.IsGreaterThan => IsGreaterThan,
-			Instructions.IsEqual => IsEqual,
-			Instructions.IsNotEqual => IsNotEqual,
-			Instructions.And => And,
-			Instructions.Or => Or,
-			
-			// Actions
-			Instructions.SetFaction => SetFaction,
-			Instructions.SetPower => SetPower,
-			Instructions.Destroy => Destroy,
-			Instructions.DealDamage => DealDamage,
-			Instructions.Draw => Draw,
-			
-			// Out of Bounds
-			_ => throw new ArgumentOutOfRangeException(nameof(instruction), instruction, "No Valid Operation")
-		};
-
-		private void Draw()
-		{
-			Player player = _players[_stack.Pop()];
-			int count = _stack.Pop();
-			
-			for (int i = 0; i < count; i++)
+			return instruction switch
 			{
-				Card card = player.Deck[player.Deck.Count - 1];
-				player.Deck.Remove(card);
-				player.Hand.Add(card);
-			}
+				// Getters
+				Instructions.Literal => Literal,
+				Instructions.GetOwningCard => GetOwningCard,
+				Instructions.GetController => GetController,
+				Instructions.GetOpponent => GetOpponent,
+				Instructions.GetDeck => GetDeck,
+				Instructions.GetGraveyard => GetGraveyard,
+				Instructions.GetHand => GetHand,
+				Instructions.GetUnits => GetUnits,
+				Instructions.GetSupport => GetSupports,
+				Instructions.Count => Count,
+
+				// Control Flow
+				Instructions.If => If,
+				Instructions.GoToEnd => GoToEnd,
+
+				// Boolean
+				Instructions.IsLessThan => IsLessThan,
+				Instructions.IsGreaterThan => IsGreaterThan,
+				Instructions.IsEqual => IsEqual,
+				Instructions.IsNotEqual => IsNotEqual,
+				Instructions.And => And,
+				Instructions.Or => Or,
+
+				// Actions
+				Instructions.SetFaction => SetFaction,
+				Instructions.SetPower => SetPower,
+				Instructions.Destroy => Destroy,
+				Instructions.DealDamage => DealDamage,
+				Instructions.Draw => Draw,
+
+				// Out of Bounds
+				_ => throw new ArgumentOutOfRangeException(nameof(instruction), instruction, "No Valid Operation")
+			};
 		}
 
-		private void DealDamage()
-		{
-			Player player = _players[_stack.Pop()];
-			int damage = _stack.Pop();
-			player.Health -= damage;
-		}
-		
-
-		private void Destroy()
-		{
-			foreach (Card card in _cards)
-			{
-				card.Zone.Remove(card);
-				card.Owner.Graveyard.Add(card);
-				card.Zone = card.Owner.Graveyard;
-			}
-		}
-
+		#region Getters
 		private void Literal()
 		{
 			_index++;
 			_stack.Push(_stack[_index]);
 		}
+		
+		private void GetOwningCard() => _cards.Add(_activated);
+		private void GetController() => _stack.Push(0);
+		private void GetOpponent() => _stack.Push(1);
+		private void GetDeck() => _cards.AddRange(_players[_stack.Pop()].Deck);
+		private void GetHand() => _cards.AddRange(_players[_stack.Pop()].Hand);
+		private void GetUnits() =>	_cards.AddRange(_players[_stack.Pop()].Units);
+		private void GetSupports() => _cards.AddRange(_players[_stack.Pop()].Supports);
+		private void GetGraveyard() => _cards.AddRange(_players[_stack.Pop()].Graveyard);
+		private void Count() => _stack.Push(_cards.Count);
+		#endregion
 
-		private void IsGreaterThan()
+		#region Control Flow
+
+		private void If()
 		{
-			const int isFalse = 0;
-            const int isTrue = 1;
-            int a = _stack.Pop();
-            int b = _stack.Pop();
-            _stack.Push(a > b ? isTrue : isFalse);
+			const int isTrue = 1;
+			// Should Jumps be Implicit?
+			int jump = _stack.Pop();
+			int success = _stack.Pop();
+			if (success == isTrue)
+			{
+        		
+			}
+			else
+			{
+				_index = jump;
+			}
 		}
 		
+		private void GoToEnd() => _index = _maxSize;
+
+		#endregion
+
+		#region Boolean Operators
+
 		private void IsLessThan()
 		{
 			const int isFalse = 0;
@@ -123,6 +120,14 @@ namespace CardGame.Server
 			int a = _stack.Pop();
 			int b = _stack.Pop();
 			_stack.Push(a < b ? isTrue : isFalse);
+		}
+		private void IsGreaterThan()
+		{
+			const int isFalse = 0;
+			const int isTrue = 1;
+			int a = _stack.Pop();
+			int b = _stack.Pop();
+			_stack.Push(a > b ? isTrue : isFalse);
 		}
 
 		private void IsEqual()
@@ -142,22 +147,6 @@ namespace CardGame.Server
 			int b = _stack.Pop();
 			_stack.Push(a != b ? isTrue : isFalse);
 		}
-
-		private void If()
-		{
-        	const int isTrue = 1;
-        	// Should Jumps be Implicit?
-        	int jump = _stack.Pop();
-        	int success = _stack.Pop();
-        	if (success == isTrue)
-        	{
-        		
-        	}
-        	else
-        	{
-        		_index = jump;
-        	}
-        }
 		
 		private void And()
 		{
@@ -176,7 +165,11 @@ namespace CardGame.Server
 			int b = _stack.Pop();
 			_stack.Push(a == isTrue || b == isTrue ? isTrue : isFalse);
 		}
-		
+
+		#endregion
+
+		#region Actions
+
 		private void SetFaction()
 		{
 			Faction faction = (Faction) _stack.Pop();
@@ -188,17 +181,46 @@ namespace CardGame.Server
 			int power = _stack.Pop();
 			foreach (Card card in _cards) { card.Power = power; }
 		}
+		
+		private void Destroy()
+		{
+			foreach (Card card in _cards)
+			{
+				card.Zone.Remove(card);
+				card.Owner.Graveyard.Add(card);
+				card.Zone = card.Owner.Graveyard;
+			}
+		}
 
-		private void GoToEnd() => _index = _maxSize;
-		private void Count() => _stack.Push(_cards.Count);
-		private void GetController() => _stack.Push(0);
-		private void GetOpponent() => _stack.Push(1);
-		private void GetDeck() => _cards.AddRange(_players[_stack.Pop()].Deck);
-		private void GetHand() => _cards.AddRange(_players[_stack.Pop()].Hand);
-		private void GetUnits() =>	_cards.AddRange(_players[_stack.Pop()].Units);
-		private void GetSupports() => _cards.AddRange(_players[_stack.Pop()].Supports);
-		private void GetGraveyard() => _cards.AddRange(_players[_stack.Pop()].Graveyard);
-		private void GetOwningCard() => _cards.Add(_activated);
+		private void DealDamage()
+		{
+			Player player = _players[_stack.Pop()];
+			int damage = _stack.Pop();
+			player.Health -= damage;
+		}
+		
+		private void Draw()
+		{
+			Player player = _players[_stack.Pop()];
+			int count = _stack.Pop();
+			
+			for (int i = 0; i < count; i++)
+			{
+				Card card = player.Deck[player.Deck.Count - 1];
+				player.Deck.Remove(card);
+				player.Hand.Add(card);
+			}
+		}
+
+		#endregion
+		
+		
+		
+		
+
+
+
+
 
 
 
