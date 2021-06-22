4e3eff0abc776d490eb01bae0fd8426d676bf817
Alex <047497501+AlexDarigan@users.noreply.github.com>
2021-06-22 09:52:51 +0100

    Add Placeholder Deck

--- Client/Room/Participant.cs
+++ Client/Room/Participant.cs
@@ -1,24 +1,24 @@
-﻿using System.IO.Ports;
+using System.IO.Ports;
 using Godot;
 
 namespace CardGame.Client
 {
-    public class Participant: Node
-    {
-        public Position3D Deck { get; private set; }
-        public Position3D Discard { get; private set; }
-        public Node Hand { get; private set; }
-        public Node Units { get; private set; }
-        public Node Support { get; private set; }
+	public class Participant: Node
+	{
+		public Position3D Deck { get; private set; }
+		public Position3D Discard { get; private set; }
+		public Node Hand { get; private set; }
+		public Node Units { get; private set; }
+		public Node Support { get; private set; }
 
-        public override void _Ready()
-        {
-            Deck = GetNode <Position3D> ("Deck");
-            Deck = GetNode <Position3D> ("Discard");
-            Hand = GetNode <Node>("Hand");
-            Units = GetNode <Node>("Units");
-            Support = GetNode <Node>("Support");
+		public override void _Ready()
+		{
+			Deck = GetNode <Position3D> ("Deck");
+			Deck = GetNode <Position3D> ("Discard");
+			Hand = GetNode <Node>("Hand");
+			Units = GetNode <Node>("Units");
+			Support = GetNode <Node>("Support");
 
-        }
-    }
+		}
+	}
 }
