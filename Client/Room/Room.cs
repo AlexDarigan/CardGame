cf262a015697a452dd1ffd0d12dc135521bcfdfb
Alex <047497501+AlexDarigan@users.noreply.github.com>
2021-06-24 01:23:39 +0100

    Add Basics of Input

--- Client/Room/Room.cs
+++ Client/Room/Room.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using Godot;
+using Array = Godot.Collections.Array;
 
 namespace CardGame.Client
 {
@@ -21,7 +23,7 @@ namespace CardGame.Client
 		// NOTE: We'll be using a lot of scattered code inside here before sorting it out later
 		[Signal] public delegate void Updated();
 		private readonly PackedScene CardScene = (PackedScene) GD.Load("res://Client/Card/Card.tscn");
-		private readonly Dictionary<int, Card> cards = new();
+		private readonly System.Collections.Generic.Dictionary<int, Card> cards = new();
 		private Spatial Table;
 		private readonly Queue<Command> CommandQueue = new();
 		private Tween GFX;
@@ -63,12 +65,13 @@ namespace CardGame.Client
 		[Puppet] public void Pass() => RpcId(Server, "Pass");
 		[Puppet] public void EndTurn() => RpcId(Server, "EndTurn");
 		
-		private Command LoadDeck(bool isClient, Dictionary<int, SetCodes> deck) => new LoadDeck(GetPlayer(isClient), deck, CreateCard);
+		private Command LoadDeck(bool isClient, System.Collections.Generic.Dictionary<int, SetCodes> deck) => new LoadDeck(GetPlayer(isClient), deck, CreateCard);
 		private Command Draw(bool isClient, int cardId) => new Draw(GetPlayer(isClient), GetCard(cardId));
 		private Command Deploy(bool isClient, int cardId) => new Deploy(GetPlayer(isClient), GetCard(cardId));
 		private Player GetPlayer(bool isClient) => isClient ? Player : Rival;
 		private Card GetCard(int id, SetCodes setCode = SetCodes.NullCard) => cards.ContainsKey(id) ? cards[id] : CreateCard(id, setCode);
-		
+
+		private Card CurrentCard;
 		private Card CreateCard(int id, SetCodes setCodes)
 		{
 			CardInfo info = Library.Cards[setCodes];
@@ -83,18 +86,28 @@ namespace CardGame.Client
 			card.Art = (Texture) GD.Load($"res://Client/Assets/CardArt/{info.Art}.png");
 			cards[id] = card;
 			card.Translation = new Vector3(0, -3, 0);
-			//card.GetNode<Area>("Area").Connect("mouse_entered", this, nameof(OnMouseEnterCard), new Array{ card });
-			//card.GetNode<Area>("Area").Connect("mouse_exited", this, nameof(OnMouseExitCard), new Array{ card });
+			card.GetNode<Area>("Area").Connect("mouse_entered", this, nameof(OnMouseEnterCard), new Array{ card });
+			card.GetNode<Area>("Area").Connect("mouse_exited", this, nameof(OnMouseExitCard), new Array{ card });
 			return card;
 		}
+
+		public void OnMouseEnterCard(Card card) { CurrentCard = card; }
+		public void OnMouseExitCard(Card card) { if (CurrentCard == card) { CurrentCard = null; } }
+
+		public override void _Input(InputEvent input)
+		{
+			if (input is InputEventMouseButton {Doubleclick: true, ButtonIndex: (int) ButtonList.Left} && CurrentCard is not null)
+			{
+				OnCardPressed();
+			}
+		}
+
+		private void OnCardPressed()
+		{
+			// Switch against Card State
+			Console.WriteLine($"{CurrentCard} pressed");
+		}
 		
-		/*
-		 * OnInput()
-		 * if input is doubleclicked left mouse button and current card != null
-		 * onCardClicked(currentCard) switch against card state
-		 */
-
-
 	}
 
 	public class Player
