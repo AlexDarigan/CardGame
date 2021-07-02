d45b6ecad2f2c3e8fd9f623985879061f17f0ac4
Alex <047497501+AlexDarigan@users.noreply.github.com>
2021-07-02 14:34:33 +0100

    Remove Redundant Methods from Room

--- Client/Scripts/Room.cs
+++ Client/Scripts/Room.cs
@@ -9,38 +9,10 @@ namespace CardGame.Client
 
     public class Room : Node
     {
-        // TODO (REWRITE)
-        // 9 - Add Basic Input Controller / Multiplayer Commands
-        //		...Draw, Deploy, Set, Activate, Destroy, Discard, End, Win, Lose
-        // 10 - Add Commands for Draw/Deploy/Set/Activate/Destroy/Discard/End/Win/Lose
-        // Etc -> Add SFX, ParticleFX, Tests, Hooks for Testing
-
-        // BASIC INPUT SYSTEM
-
-        // ACTIONS
-        // ..Activate
-        // ..AttackUnit
-        // ..AttackPlayer
-        // ..PassPlay
-        // ..EndTurn
-        // NOTE: Focus on simplest first? (Pass/End)
-
-        // PRE-REQUISITES
-        // ..PlayerState
-        // ..CardState
-        // ..CardUpdate
-        // ..Targets for Activation/Attack
-
-        // MUSINGS
-        // ..Should we change zones to be selectable?
-        // ..Does positioning benefit us?
-        // ..It makes sense considering how our zones look
-
-        [Signal]
-        public delegate void Updated();
+        
+        [Signal] public delegate void Updated();
 
         private const int Server = 1;
-
         private readonly Cards _cards;
         private readonly Queue<Command> _commandQueue = new();
         private readonly Tween _gfx;
@@ -63,83 +35,50 @@ namespace CardGame.Client
             GD.Print("Room Created");
         }
 
-        public override void _Ready()
-        {
-            RpcId(Server, "OnClientReady");
-        }
-
-        [Puppet]
-        public void Queue(CommandId commandId, params object[] args)
-        {
-            _commandQueue.Enqueue((Command) Call(commandId.ToString(), args));
-        }
-
+        public override void _Ready() => RpcId(Server, "OnClientReady");
+        
         [Puppet]
         public async void Update(States states)
         {
             while (_commandQueue.Count > 0) await _commandQueue.Dequeue().Execute(_gfx);
             _player.Update(states);
-            EmitSignal(nameof(Updated), states);
+            EmitSignal(nameof(Updated), states); 
         }
-
-        [Puppet]
-        public void UpdateCard(int id, CardState state)
-        {
-            _cards[id].Update(state);
-        }
-
-        [Puppet]
-        private void Deploy(Card card)
-        {
-            RpcId(Server, "Deploy", card.Id);
-        }
-
-        [Puppet]
-        private void Set(Card card)
-        {
-            RpcId(Server, "Set", card.Id);
-        }
-
-        [Puppet]
-        public void Pass()
-        {
-            RpcId(Server, "Pass");
-        }
-
-        [Puppet]
-        public void EndTurn()
-        {
-            RpcId(Server, "EndTurn");
-        }
-
-        private Command LoadDeck(bool isClient, Dictionary<int, SetCodes> deck)
-        {
-            return new LoadDeck(GetPlayer(isClient), deck, _cards.GetCard);
-        }
-
-        private Command Draw(bool isClient, int cardId)
-        {
-            return new Draw(GetPlayer(isClient), GetCard(cardId));
-        }
-
-        private Command Deploy(bool isClient, int cardId)
-        {
-            return new Deploy(GetPlayer(isClient), GetCard(cardId));
-        }
-
-        private Command SetFaceDown(bool isClient, int cardId)
-        {
-            return new Set(GetPlayer(isClient), GetCard(cardId));
-        }
-
-        private Participant GetPlayer(bool isClient)
-        {
-            return isClient ? _player : _rival;
-        }
-
-        private Card GetCard(int id, SetCodes setCode = SetCodes.NullCard)
-        {
-            return _cards.GetCard(id, setCode);
-        }
+        
+        [Puppet] public void Queue(CommandId commandId, params object[] args) => _commandQueue.Enqueue((Command) Call(commandId.ToString(), args)); 
+        [Puppet] public void UpdateCard(int id, CardState state) => _cards[id].Update(state); 
+        private Command LoadDeck(bool who, Dictionary<int, SetCodes> deck) => new LoadDeck(GetPlayer(who), deck, _cards.GetCard); 
+        private Command Draw(bool who, int id) => new Draw(GetPlayer(who), GetCard(id)); 
+        private Command Deploy(bool who, int id) => new Deploy(GetPlayer(who), GetCard(id)); 
+        private Command SetFaceDown(bool who, int id) => new Set(GetPlayer(who), GetCard(id)); 
+        private Participant GetPlayer(bool isClient) => isClient ? _player : _rival; 
+        private Card GetCard(int id, SetCodes setCode = SetCodes.NullCard) => _cards.GetCard(id, setCode); 
     }
 }
+
+// TODO (REWRITE)
+// 9 - Add Basic Input Controller / Multiplayer Commands
+//		...Draw, Deploy, Set, Activate, Destroy, Discard, End, Win, Lose
+// 10 - Add Commands for Draw/Deploy/Set/Activate/Destroy/Discard/End/Win/Lose
+// Etc -> Add SFX, ParticleFX, Tests, Hooks for Testing
+
+// BASIC INPUT SYSTEM
+
+// ACTIONS
+// ..Activate
+// ..AttackUnit
+// ..AttackPlayer
+// ..PassPlay
+// ..EndTurn
+// NOTE: Focus on simplest first? (Pass/End)
+
+// PRE-REQUISITES
+// ..PlayerState
+// ..CardState
+// ..CardUpdate
+// ..Targets for Activation/Attack
+
+// MUSINGS
+// ..Should we change zones to be selectable?
+// ..Does positioning benefit us?
+// ..It makes sense considering how our zones look
