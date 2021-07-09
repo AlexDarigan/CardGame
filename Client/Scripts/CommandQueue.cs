06944bfa3d890cebe54d16e684792b8821b1baa5
Alex <047497501+AlexDarigan@users.noreply.github.com>
2021-07-09 12:23:36 +0100

    Merge CommandQueue Into Tween

--- Client/Scripts/CommandQueue.cs
+++ Client/Scripts/CommandQueue.cs
@@ -1,7 +1,43 @@
-﻿namespace CardGame.Client
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+namespace CardGame.Client.Commands
 {
-    public class CommandQueue
+    public class CommandQueue: Tween
     {
+        private Queue<Command> Queue { get; } = new();
+        private Player Player { get; }
+        private Rival Rival { get; }
+        private Cards Cards { get; }
+        public int Count => Queue.Count;
         
+        public CommandQueue(Player player, Rival rival, Cards cards)
+        {
+            Player = player;
+            Rival = rival;
+            Cards = cards;
+        }
+
+        public void Enqueue(CommandId commandId, params object[] args)
+        {
+            Queue.Enqueue((Command) Call(commandId.ToString(), args));
+        }
+
+        public Command Dequeue()
+        {
+            return Queue.Dequeue(); //.Execute(this);
+        }
+        
+        private Participant GetPlayer(bool isPlayer) { return isPlayer ? Player : Rival; }
+        private Card GetCard(int id, SetCodes setCodes = SetCodes.NullCard) { return Cards.GetCard(id, setCodes);}
+        
+        private Command LoadDeck(bool who, Dictionary<int, SetCodes> deck) { return new LoadDeck(GetPlayer(who), deck, Cards.GetCard);} 
+        private Command Draw(bool who, int id) { return new Draw(GetPlayer(who), GetCard(id)); }
+        private Command Deploy(bool who, int id, SetCodes setCodes) { return new Deploy(GetPlayer(who), GetCard(id, setCodes));}
+        private Command SetFaceDown(bool who, int id) { return new SetFaceDown(GetPlayer(who), GetCard(id));}
+        private Command SetHealth(bool who, int health) { return new SetHealth(GetPlayer(who), health);}
+        private Command SentToGraveyard(int id) { return new SentToGraveyard(GetCard(id));}
+        private Command Battle(int attacker, int defender) { return new Battle(GetCard(attacker), GetCard(defender));}
     }
 }
--- Client/Scripts/Room.cs
+++ Client/Scripts/Room.cs
@@ -12,8 +12,9 @@ namespace CardGame.Client
         public event EventHandler GameUpdated;
         private const int Server = 1;
         private Cards Cards { get; }
-        private Queue<Command> CommandQueue { get; } = new();
-        private Tween Gfx { get; }
+       // private Queue<Command> CommandQueue { get; } = new();
+      //  private Tween Gfx { get; }
+        private CommandQueue CommandQueue { get; }
         private AudioStreamPlayer Sfx { get; }
         private AudioStreamPlayer Bgm { get; }
         private Control Gui { get; }
@@ -25,52 +26,62 @@ namespace CardGame.Client
         {
             Name = name;
             CustomMultiplayer = multiplayerApi;
-        
-            Gfx = new Tween();
+
+            Mouse mouse = new Mouse();
+            Player = new Player(mouse);
+            Rival = new Rival();
             Sfx = new AudioStreamPlayer();
             Bgm = new AudioStreamPlayer();
             Cards = new Cards();
-            Mouse mouse = new Mouse();
-            
-            foreach (Node child in new []{view, Gfx, Sfx, Bgm, Cards, mouse}) { AddChild(child, true); }
+            CommandQueue = new CommandQueue(Player, Rival, Cards);
+
+            foreach (Node child in new []{view, CommandQueue, Sfx, Bgm, Cards, mouse}) { AddChild(child, true); }
             
             Gui = view.GetNode<Control>("GUI");
             Gui.GetNode<Button>("Menu/EndTurn").Connect("pressed", this, nameof(OnEndTurnPressed));
             Gui.GetNode<Label>("ID").Text = multiplayerApi.GetNetworkUniqueId().ToString();
             
-            Player = new Player(mouse);
-            Rival = new Rival();
+            
             Player.Declare += Declare;
             Cards.Player = Player;
         }
-        
-        public override void _Ready() => RpcId(Server, "OnClientReady");
+
+        public override void _Ready() { RpcId(Server, "OnClientReady"); }
 
         private void Declare(CommandId command, params object[] args) { RpcId(Server, command.ToString(), args); }
         
         [Puppet]
         public async void Update(States state)
         {
-            while (CommandQueue.Count > 0) await CommandQueue.Dequeue().Execute(Gfx);
+            while (CommandQueue.Count > 0) await CommandQueue.Dequeue().Execute(CommandQueue);
             Player.State = state;
             Gui.GetNode<Label>("State").Text = state.ToString();
             GameUpdated?.Invoke(null, null);
         }
+
+        [Puppet]
+        public void Queue(CommandId commandId, params object[] args)
+        {
+            CommandQueue.Enqueue(commandId, args);
+        }
+
+        [Puppet]
+        public void UpdateCard(int id, CardState state)
+        {
+            Cards[id].Update(state);
+        }
         
-        [Puppet] public void Queue(CommandId commandId, params object[] args) => CommandQueue.Enqueue((Command) Call(commandId.ToString(), args)); 
-        [Puppet] public void UpdateCard(int id, CardState state) => Cards[id].Update(state); 
-        private Command LoadDeck(bool who, Dictionary<int, SetCodes> deck) => new LoadDeck(GetPlayer(who), deck, Cards.GetCard); 
-        private Command Draw(bool who, int id) => new Draw(GetPlayer(who), GetCard(id)); 
-        private Command Deploy(bool who, int id, SetCodes setCodes) => new Deploy(GetPlayer(who), GetCard(id, setCodes));
-        private Command SetFaceDown(bool who, int id) => new SetFaceDown(GetPlayer(who), GetCard(id));
-        private Command SetHealth(bool who, int health) => new SetHealth(GetPlayer(who), health);
-        private Command SentToGraveyard(int id) => new SentToGraveyard(GetCard(id));
-        private Command Battle(int attacker, int defender) => new Battle(GetCard(attacker), GetCard(defender));
-        private Participant GetPlayer(bool isClient) => isClient ? Player : Rival; 
-        private Card GetCard(int id, SetCodes setCode = default) => Cards.GetCard(id, setCode);
+        
+        // private Participant GetPlayer(bool isClient) { return isClient ? Player : Rival; }
+        // private Card GetCard(int id, SetCodes setCode = default) { return Cards.GetCard(id, setCode);}
         public void OnEndTurnPressed() { Player.EndTurn(); }
     }
 }
+
+// GettingPlayer
+// GettingCards
+// GetPlayer, GetCards, Arguments, can system reflection take variable arguments?
+// Can System Reflection STORE commands variable arguments?
 
 // TODO (REWRITE)
 // 9 - Add Basic Input Controller / Multiplayer Commands
